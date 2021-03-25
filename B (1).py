280a1763f3583990c7b9a8aae83d07cf63ff5aa8
Eduardo <martinez.romero.eduardo@gmail.com>
2021-03-25 10:36:33 +0100

    Realizados comentarios de correcion

--- B (1).py
+++ B (1).py
@@ -1,3 +1,12 @@
+"""
+En general:
+    · No se usan los mecanismos de encapsulación, \
+    principalmente los de visibilidad, por lo que \
+    algunos métodos sensibles, como los setters, \
+    son accesibles, haciendo a los atributos \
+        susceptibles de cambios inesperados.
+    ·
+"""
 from abc import ABC, abstractmethod
 
 #ejercico 1
@@ -46,9 +55,9 @@ class Alumno:
         for i in self.asignaturas:
             if i == asig:
                 return i
-
+            # Este "aire" -
             else:
-
+            # - no debería estar.
                 raise ValueError('NO tienes esa asignatura')
 
 
@@ -65,7 +74,7 @@ class Alumno:
         return suma/asignatura.get_tri()
 
     def nota(self, asig, tri):
-        return self.busca_asig(asig).nota[tri-1]
+        return self.busca_asig(asig).notas[tri-1]    #a notas le faltaba la "s"
 
     def aprobada(self ,asig):
         if self.media(asig) >= 5:
@@ -76,25 +85,78 @@ class Alumno:
 
 class Grupo:
     def __init__(self) -> None:
-        self.grupo = []
+        # Quizá alumnos hubiese sido un nombre más adecuado.
+        self.grupo = [] #self.__alumnos = []
+
+    """
+    Deberíamos tener accesores y mutadores que garantizen la encapsulación.
+    Ej:
+    def __get_alumnos(self):
+        return self.__grupo
+
+    def __set_alumno(self, alumno):
+        if isinstance(alumno, Alumno):
+            self.__alumno.append(alumno)
+        else: raise TypeError('Los grupos sólo almacenan alumnos.')
+    """
 
     def anyadir_alumno(self, alumno):
-        self.grupo.append(alumno)
+        """
+        Debería haber un setter que hiciese esta operación, \
+        y supongo, porque no tengo los enunciados, que ese \
+        setter debe controlar que sólo pudiese recibir \
+        instancias de la clase Alumno.
+        """
+        self.grupo.append(alumno) # self.__set_alumno(alumno)
 
 
 
 class Asignatura:
-    def __init__(self, denominacio, numtri) -> None:
+    """
+    Podrías añadir un valor por defecto al número de \
+    trimestres. Después de todo casi siempre se espera \
+    que sean tres.
+    """
+    def __init__(self, denominacio, numtri = 3) -> None:
         self.denom = denominacio
         self.numtri = numtri
         self.notas = []
-        for x in range(0, self.numtri):
+        for x in range(self.numtri):
             self.notas.append(0)
 
     def get_tri(self):
         return self.numtri
 
+"""
+Sería más apropiado que los test estuviesen preparados \
+para ser ejecutados si ejecutamos este modulo como \
+programa principal. Usando:
+
+if __name == '__main__':
+
+Y luego indentamos las siguientes lineas para que estén \
+dentro de esa estructura if.
+"""
 juan = Alumno("Juan")
 ingles = Asignatura("ingles", 3)
 juan.matricular(ingles)
 juan.set_nota(ingles, 1, 1).set_nota(ingles, 2, 3).set_nota(ingles, 3, 6)
+
+"""
+Y si yo creo ahora otro alumno, lo matriculo en inglés, \
+y le pongo nota, ¿que va a suceder?
+"""
+paco = Alumno('Paco')
+paco.matricular(ingles)
+print(f'Recordemos que {juan.nombre} tiene un {juan.nota(ingles, 1)} en '\
+    f'el primer trimestre de {juan.busca_asig(ingles).denom}')
+paco.set_nota(ingles, 1, 8).set_nota(ingles, 2, 6).set_nota(ingles, 3, 7)
+print(f'Si creo a un nuevo alumno que se llama {paco.nombre}, lo matriculo '\
+    f'también en {paco.busca_asig(ingles).denom} y le pongo notas...')
+print()
+print(f'Observa que le pasa a la nota de {juan.nombre}: {juan.nota(ingles, 1)}')
+print()
+print('El modelado que realizaste para almacenar las notas de los alumnos '\
+    'no está funcionando como debería, \npuesto que al poner nota en '\
+    f'{ingles.denom} a {paco.nombre}, estamos machacando la nota del último '\
+    f'alumno matriculado antes, \nen este caso es {juan.nombre}.')
